b0d809f79f8d9f8055763c71341dcd2898f6afb8
Eva Blanco <51144190+Evablan@users.noreply.github.com>
2025-03-18 14:03:15 +0100

    He creado blog.php y lo he vinculado con su hoja de estilos blog.css. También he sacado de la barra de navegación los elementos que sobraban y ahora está actualizada

---
+++ vistas/blog.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+    <link rel="stylesheet" href="css/blog.css">
+</head>
+
+<body>
+    <div class="contenedor-blog">
+        <h2>Mi Blog personal</h2>
+        <input type="text" name="titulo" placeholder="Titulo" required><br><br>
+        <textarea name="contenido" placeholder="Escribe tu post..." required></textarea><br><br>
+        <button type="submit">Publicar</button>
+
+
+
+
+    </div>
+
+
+</body>
+
+</html>
--- vistas/includes/header.php
+++ vistas/includes/header.php
@@ -2,8 +2,6 @@
     <nav>
         <ul>
             <li><a href="quienes_somos.php"><i class="fa-solid fa-users"></i>Quiénes Somos</a></li>
-            <li><a href="formas_de_dar.php"><i class="fa-solid fa-hand-holding-heart"></i>Formas de dar</a></li>
-            <li><a href="eventos.php"><i class="fa-solid fa-calendar-days"></i>Eventos y recaudación de fondos</a></li>
             <li><a href="Contacto.php"><i class="fa-solid fa-envelope"></i><span style="color:pink;">Contacto</span></a></li>
             <li><a href="vistas/registro.php"> <i class="fa-solid fa-user-plus"></i>Registrate</a></li>
 
